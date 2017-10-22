cbbda7ab61065f30996c6cd2901019ebea6cb542
Ilya <dyavolick@gmail.com>
2017-10-22 17:19:50 +0300

    fix bot

--- TelegramLotteryBot/bot.py
+++ TelegramLotteryBot/bot.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-import config
+
 import telebot
+import config
+
 bot = telebot.TeleBot(config.token)
+
 @bot.message_handler(content_types=["text"])
-def  repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message): # Название функции не играет никакой роли, важно не повторяться
     bot.send_message(message.chat.id, message.text)
-    if __name__ == '__main__':
-        bot.polling(none_stop=True)
 
-
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
